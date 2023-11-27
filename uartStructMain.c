463044a5895f55c2a36cb5bf7bbd5f9f1759f4b2
anoopjayaram <anoopjayaram97@gmail.com>
2023-11-27 12:44:16 +0530

    added header files

--- uartStructMain.c
+++ uartStructMain.c
@@ -7,30 +7,13 @@
 
 #include "uart.h"
 
-// Function to wait until TXIF flag is set
-void UART_WaitForTxComplete() {
-    while (!PIR1bits.TXIF) {
-        continue; // Wait until TXIF flag is set
-    }
-}
-// Function to transmit a string via UART
-void UART_TransmitString(const char* data) {
-    while (*data != '\0') {
-        // Wait for TXIF flag indicating the transmit buffer is empty
-        while (!TXSTAbits.TRMT) {
-            continue;
-        }
-        TXREG = *data;
-        data++; 
-    }
-}
 void main() {
      TRISC=0X00;
     TXSTA_Config txConfig; 
     txConfig.txEnable = 1; // Enable transmit
     txConfig.syncMode = 0; // Async mode
     txConfig.highSpeed = 1;
-    
+  
     RCSTA_Config rxConfig; 
     rxConfig.SerialPortEnable = 1; // Enable transmit
     rxConfig.continiousReciveEnable = 1; // Async mode
